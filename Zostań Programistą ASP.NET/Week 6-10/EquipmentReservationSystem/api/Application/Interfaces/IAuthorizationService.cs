48daf02475dae2666410f5ceb2f6ede17937fca9
Digital-Forge <jan.m.szkudlarek@gmail.com>
2024-01-31 12:41:37 +0100

    Add AuthorizationService

---
+++ Zostań Programistą ASP.NET/Week 6-10/EquipmentReservationSystem/api/Application/Interfaces/IAuthorizationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces
+{
+    public partial interface IAuthorizationService
+    {
+        Task<string> Login(ILoginModel model);
+        Task<string> GenerateJsonWebToken(ILoginModel model);
+        Task Logout();
+    }
+}
